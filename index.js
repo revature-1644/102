6dcf8681db09888f552d4bebe4b27ed640ce52ea
Theodore Balashov <theodore.balashov@revature.com>
2023-10-02 14:16:24 -0400

    js demo

---
+++ index.js
@@ -0,0 +1,38 @@
+console.log(document);
+console.log(document.getElementById("ptag"))
+let element = document.getElementById("ptag");
+element.innerText = "javascript";
+let button = document.getElementById("buttonTag");
+button.onclick = buttonClicked;
+let input = document.getElementById("inputTag")
+let ol = document.getElementById("olTag");
+let data = [];
+/**
+ * Every time the button is clicked, take the
+ * current contents of the input box,
+ * and add them to the array. Then, display
+ * the array on the site... somehow
+ */
+function buttonClicked(){
+// take value from input box
+    let value = input.value;
+    console.log(value);
+// put value in the array
+    data.push(value);
+    console.log(data);
+    populateList();
+}
+
+
+function populateList(){
+    ol.innerHTML = "";
+    // for every item in the data array,
+    // create a new html element
+    // set its internal text value to the data value
+    // append the new html to the ol list
+    for(let i = 0; i < data.length; i++){
+        let listItem = document.createElement("li");
+        listItem.innerText = data[i];
+        ol.appendChild(listItem);
+    }
+}
